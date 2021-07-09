0c283d3c4e8a316c134d23be90830202ed7aa065
Patrick Huesmann <info@patrick-huesmann.de>
2021-07-09 19:20:23 +0200

    Add Debian package generation

--- dkms-run.sh
+++ dkms-run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "Skipping dkms build/install w/o root privileges."
+   exit 0
+fi
+
 # Remove module in case it was installed previously
 dkms remove xdma/${XDMA_VER_STR} --all
 
